f0563f96aa2e3ec2c0a71b745a07f98c3751b809
Aidan <aidanpauldavis@yahoo.co.uk>
2019-12-01 01:41:25 +0000

    Refactored components, introduced new components for modularised navigation. hooked up initial blog to test things

--- techblog/src/components/Home.vue
+++ techblog/src/components/Home.vue
@@ -8,39 +8,20 @@
      <p class="banner-intro">You are invited to read through this Blog to discover and follow along on my journey through the crazy world of tech. From walkthroughs and guides to what I've been upto recently, join my side and lets grow together!</p>
     </div>
     <div class="content-main-blog-landing">
-
-        <div class="article">
-            <img src="https://images.unsplash.com/photo-1542546068979-b6affb46ea8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" class="article-image"/>
-            <div class="article-content">
-                <h3><span>Front-end hero</span></h3>
-                <span>Author(s) - Kyle Colquhoun<a href="/pages/frontendhero/chapter1.html">Click To Read</a></span>
-            </div>
-            <div class="article-buttons">
-                <div class="buttons" id="like-button"><i class="far fa-heart"></i></div>
-                <div class="buttons"><i class="far fa-comment"></i></div>
-                <div class="buttons"><i class="far fa-share-square"></i></div>
-            </div>
-        </div>
+        <article-component authors="Kyle Colquhoun" title="Front-end Hero" path="chapter1"/>
+        <article-component authors="Kyle Colquhoun" title="Hello world" path="helloworld"/>
         
-    <div class="article">
-        <img src="https://images.unsplash.com/photo-1542546068979-b6affb46ea8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" class="article-image"></img>
-        <div class="article-content">
-            <h3><span>Hello World!</span></h3>
-            <span>Author(s) - Kyle Colquhoun<a href="/pages/kyle/aboutme.html">Click To Read</a></span>
-        </div>
-        <div class="article-buttons">
-        <div class="buttons" id="like-button"><i class="far fa-heart"></i></div>
-        <div class="buttons"><i class="far fa-comment"></i></div>
-        <div class="buttons"><i class="far fa-share-square"></i></div>
-    </div>
-</div>
 
 </div>
   </div>
 </template>
 
 <script>
+import articleComponent from './ui/article'
 export default {
+    components: {
+        articleComponent
+    }
 
 }
 </script>
---
+++ techblog/src/components/ui/article.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="article">
+        <img src="https://images.unsplash.com/photo-1542546068979-b6affb46ea8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" class="article-image" />
+        <div class="article-content">
+            <h3><span>{{title}}</span></h3>
+            <span>Author(s) - {{authors}}
+                <navlink :path="path" text="Click here"/>
+            </span>
+        </div>
+        <div class="article-buttons">
+            <div class="buttons" id="like-button"><i class="far fa-heart"></i></div>
+            <div class="buttons"><i class="far fa-comment"></i></div>
+            <div class="buttons"><i class="far fa-share-square"></i></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import navlink from '../navigation/navlink'
+export default {
+    props: ['authors', 'title', 'path'],
+    components: {
+        navlink
+    },
+    beforeMount() {
+        console.log(1)
+    }
+
+}
+</script>
+
+<style>
+
+</style>
